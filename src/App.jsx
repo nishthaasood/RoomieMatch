a0849125b4f76e65376dc72c470f1832ab055660
nishthaasood <soodnishtha12462@gmail.com>
2025-08-30 23:44:40 +0530

    updated home, signup, login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Navbar'
+import HomePage from './HomePage'
+import Footer from './Footer'
+import LoginPage from './Login'
+import SignupPage from './Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState('home');
 
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'login':
+        return <LoginPage setCurrentPage={setCurrentPage} />;
+      case 'signup':
+        return <SignupPage setCurrentPage={setCurrentPage} />;
+      default:
+        return <HomePage setCurrentPage={setCurrentPage} />;
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="app">
+      {renderPage()}
+    </div>
+  );
 
+}
 export default App
